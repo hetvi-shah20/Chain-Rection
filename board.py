2bb388fd4c8c438e01ea1dde92b0b05259c625d2
Parth Chauhan <parthchauhan05@gmail.com>
2018-10-10 15:25:43 +0530

    2 Player Game

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 class Board:
     '''This class will create a game board to play game'''
     board = list()
-    row, column = 10, 8
+    row, column = 10, 10
 
     def __init__(self):
         for i in range(self.row):
@@ -19,3 +19,70 @@ class Board:
             pRows.append(temp)
         pBoard = '\n'.join(pRows)
         return pBoard
+
+
+    def positive_move(self,row,column):
+        if self.board[row][column] >= 0:
+            self.board[row][column] += 1
+            return self.board[row][column]
+        else:
+              return 9# for wrong move it will ask for new position
+
+
+    def negative_move(self,row,column):
+        if self.board[row][column] <= 0:  # player 2
+            self.board[row][column] -= 1
+            return self.board[row][column]
+        else:
+            return 9
+
+
+    def find_neighbour(self,row,column):
+        neighbours = list()
+        if row-1>=0:
+            neighbours.append((row-1,column))
+        if column-1>=0:
+            neighbours.append((row, column-1))
+        if row+1<self.row:
+            neighbours.append((row+1, column))
+        if column+1<self.column:
+            neighbours.append((row, column+1))
+        return neighbours
+
+
+    def positive_expand(self, x, y):
+        points = list()
+        points.append((x,y))
+        while len(points) > 0:
+            row, column = points[0]
+            neighbours = self.find_neighbour(row, column)
+            if self.board[row][column] == len(neighbours):
+                self.board[row][column] = 0
+                for i in neighbours:
+                    self.board[i[0]][i[1]] = abs(self.board[i[0]][i[1]])+1
+
+                points.extend(neighbours)
+            points.remove((row, column))
+        return
+
+    def negative_expand(self, x, y):
+        points = list()
+        points.append((x, y))
+        while len(points) > 0:
+            row, column = points[0]
+            neighbours = self.find_neighbour(row, column)
+            if abs(self.board[row][column]) == len(neighbours):
+                self.board[row][column] = 0
+                for i in neighbours:
+                    self.board[i[0]][i[1]] = (abs(self.board[i[0]][i[1]]) + 1) * -1
+
+                points.extend(neighbours)
+            points.remove((row, column))
+        return
+
+
+
+###hetvi  shah
+
+
+##hetvi
--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import board
 
 game_board = board.Board()
+while True:
+    x, y = input("[Player 1] Enter Values for Row and Column of your move separated by space").split(" ")
+    x, y = int(x), int(y)
+    move_value = game_board.positive_move(x,y)
+    game_board.positive_expand(x, y)
+    print(game_board)
+    x, y = input("[Player 2]Enter Values for Row and Column of your move separated by space").split(" ")
+    x, y = int(x), int(y)
+    move_value = game_board.negative_move(x, y)
+    game_board.negative_expand(x, y)
+    print(game_board)
+    n = input("q for quitting the game")
+    if n == "q":
+        break
+
 print(game_board)
