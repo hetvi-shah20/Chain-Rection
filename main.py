17c6fd29ed252669208b4a8fde41dbd190a3c0bd
parth <parthchauhan05@gmail.com>
2018-10-10 15:45:18 +0530

    Fix

--- main.py
+++ main.py
@@ -5,11 +5,22 @@ while True:
     x, y = input("[Player 1] Enter Values for Row and Column of your move separated by space").split(" ")
     x, y = int(x), int(y)
     move_value = game_board.positive_move(x,y)
+    while(move_value == 9):
+        print("Entered valued are ocupied by other player")
+        x, y = input("[Player 1] Enter Values for Row and Column of your move separated by space").split(" ")
+        x, y = int(x), int(y)
+        move_value = game_board.positive_move(x, y)
+
     game_board.positive_expand(x, y)
     print(game_board)
     x, y = input("[Player 2]Enter Values for Row and Column of your move separated by space").split(" ")
     x, y = int(x), int(y)
     move_value = game_board.negative_move(x, y)
+    while(move_value==9):
+        print("Entered valued are ocupied by other player")
+        x, y = input("[Player 2]Enter Values for Row and Column of your move separated by space").split(" ")
+        x, y = int(x), int(y)
+        move_value = game_board.negative_move(x, y)
     game_board.negative_expand(x, y)
     print(game_board)
     n = input("q for quitting the game")
